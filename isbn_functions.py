a921fbd7885ccf3f19651592a8a8385b71e3d489
RandyCoffman <randy.coffman@wvjc.edu>
2018-10-22 22:26:06 -0400

    python version of my isbn validation functions

---
+++ isbn_functions.py
@@ -0,0 +1,61 @@
+import re
+
+def look_at_num(number):
+    regex = re.compile('^[xX\d+]*$')
+    number = filter(regex.match, number)
+    numbers = list(number)
+    return numbers
+
+def validate_isbn10(number):
+  if number[-1] == 'x' or number[-1] == 'X':
+    number[-1] = 10
+    number = [ int(x) for x in number ]
+    counter = 10
+    new_arr = []
+    for i in number:
+      x = i * counter
+      counter -= 1
+      new_arr.append(x)
+    if sum(new_arr) % 11 == 0:
+      print(True)
+    else:
+      print(False)
+
+def validate_isbn13(number):
+  number = [ int(x) for x in number ]
+  check_digit = number.pop()
+  new_arr = []
+  print(number)
+  for index, value in enumerate(number):
+    if index+1 % 2 == 0:
+      print(index)
+      x = int(value) * 1
+      new_arr.append(x)
+    else:
+      x = int(value) * 3
+      print(index)
+      new_arr.append(x)
+  index = index+1
+  num = sum(new_arr)
+  print(num)
+  final = num % 10
+  final = 10 - final
+  print(final)
+  print(check_digit)
+  while True:
+    if final <= 9:
+      final = final % 10
+      break
+    if check_digit == final:
+      print(True)
+    else:
+      print(False)
+
+def isbn10_or_isbn13(number):
+  numbers = look_at_num(number)
+  if len(numbers) == 13:
+    validate_isbn13(numbers)
+  elif len(numbers) == 10:
+    validate_isbn10(numbers)
+  else:
+    return "sorry not a number"
